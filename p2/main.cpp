d0db41f140b4a2d35b7df6d0da3813169a3886c9
iamfatima <marifati.camgon@gmail.com>
2021-05-17 22:13:40 +0100

    erased useless stuff

--- p2/main.cpp
+++ p2/main.cpp
@@ -3,21 +3,10 @@
 
 using namespace std;
 
-class Process
-{
-    public;
-
-    int costX;
-    int costY;
-
-}
 
 int main()
 {
     int n, k; //n-> number of tasks, k-> matrix inputs
     vector<Process> processes;
-
-    scanf("%d %d", &n, &k);
-
     
 }
