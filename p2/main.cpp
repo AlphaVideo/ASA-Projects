7bd02063adc4122f4928d13c5556c65d43d7c250
iamfatima <marifati.camgon@gmail.com>
2021-05-17 21:44:06 +0100

    F moment

---
+++ p2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Process
+{
+    public;
+
+    int costX;
+    int costY;
+
+}
+
+int main()
+{
+    int n, k; //n-> number of tasks, k-> matrix inputs
+    vector<Process> processes;
+
+    scanf("%d %d", &n, &k);
+
+    
+}
