df10c09108c8bc663b49090893af33fee58be343
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2021-05-18 23:24:33 +0100

    Implemented stuff

--- p2/main.cpp
+++ p2/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <utility>
 
 using namespace std;
+
+// ~*~*~*~*~*~*~*~*~* Constants *~*~*~*~*~*~*~*~*~*~
+
+enum Direction {NONE, UV, VU};
+
+//Source and target indexes
+int source = 0; //Processor X
+int target = 0; //Processor Y
+
+#define NIL -1
 
 // ~*~*~*~*~*~*~*~*~* Classes *~*~*~*~*~*~*~*~*~*~
 
@@ -11,16 +23,25 @@ class Edge
 
     int capacity;
     int flow;
-    int startVertex;
-    int endVertex;
+    int u;
+    int v;
+    Direction d;
+    
 
-    Edge(int u, int v, int c)
+    Edge(int vertexU, int vertexV, int c)
     {
         flow = 0;
-        startVertex = u;
-        endVertex = v;
+        u = vertexU;
+        v = vertexV;
         capacity = c;
+        d = NONE;
     }
+
+    void pushFlow(int n, Direction dir);
+    bool canPush(Direction dir);
+    int getAvailableFlow(Direction dir);
+    Direction getDirection(int start);
+    int getOppositeEnd(int vertex);
 
 };
 
@@ -28,7 +49,7 @@ class Node
 {
     public:
 
-    vector<Edge> edges; //Stores child edges
+    vector<int> edges; //Stores neighbour edge indexes
 
     Node()
     {
@@ -42,12 +63,15 @@ class Graph
 
     int V;
     int E;
+    int nextEdge;
     vector<Node> nodes;
+    vector<Edge> edges;
 
     Graph(int v, int e)
     {
         V = v;
         E = e;
+        nextEdge = 0;
 
         for(int i = 0; i < V; i++)
         {
@@ -56,11 +80,14 @@ class Graph
     }
 
     void addEdge(int u, int v, int c);
+    void pushPathFlow(pair<int,int> path[], int n);
 };
 
-//Source and target indexes
-int source = 0; //Processor X
-int target = 0; //Processor Y
+// ~*~*~*~*~*~*~*~*~* Functions *~*~*~*~*~*~*~*~*~*~
+
+bool BFS(Graph g, pair<int, int> path[]);
+int edmondsKarp(Graph g);
+int findPathMinimum(Graph g, pair<int, int> path[]);
 
 int main()
 {
@@ -82,6 +109,7 @@ int main()
         graph.addEdge(i, target, y); //Task i has an edge to processor Y with weight y;
     }
 
+    //Adds inter-task edges
     for(int i = 1; i < k+1; i++)
     {
         int u, v, c;
@@ -89,20 +117,214 @@ int main()
         graph.addEdge(u, v, c);
     }
 
-    // for(int i = 0; i < graph.V; i++)
+    // for(Edge e : graph.edges)
     // {
-    //     for(Edge e : graph.nodes[i].edges)
-    //     {
-    //         printf("Start: %d  End: %d   Capacity:%d\n", e.startVertex, e.endVertex, e.capacity);
-    //     }
+    //     printf("Start: %d  End: %d   Capacity:%d   Flow:%d   Direction:%d\n", e.u, e.v, e.capacity, e.flow, e.d);
     // }
 
+    // int path[graph.V];
+
+    // if(BFS(graph, path))
+    //     puts("Yay");
+    // else
+    //     puts("Nay");
+    
+    // for(int i: path)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    int minimumCost = edmondsKarp(graph);
+    printf("%d\n", minimumCost);
+        
     return 0;
 }
+
+
 
 //Adds edge to the according vertex in the Graph class
 void Graph::addEdge(int u, int v, int c)
 {
     Edge edge(u, v, c);
-    nodes[u].edges.push_back(edge);
+    edges.push_back(edge);
+    nodes[u].edges.push_back(nextEdge);
+    nodes[v].edges.push_back(nextEdge++);
 }
+
+//Adds flux to the edge, dependant on the direction
+void Edge::pushFlow(int n, Direction dir)
+{
+    if(d == NONE) //Init
+    {
+        flow += n;
+        d = dir;
+    }
+    else if(d == dir) //Same direction
+    {
+        flow += n;
+    }
+    else //Opposite directions
+    {
+        if(flow - n < 0) //Direction changes after flow update
+        {
+            d = dir;
+            flow = abs(flow - n);
+        }
+        else //Direction remains the same
+        {
+            flow -= n;
+        }
+    }
+}
+
+//Returns true if edge not full
+bool Edge::canPush(Direction dir)
+{
+    if(dir == d)
+    {
+        if(flow == capacity)
+            return false;
+        else
+            return true;
+    }
+    else
+        return true;
+}
+
+//Get the flow that you can push through the edge
+int Edge::getAvailableFlow(Direction dir)
+{
+    if(dir == d)
+        return capacity - flow;
+    else if (dir != d)
+        return capacity + flow;
+    else //NONE == Not used yet
+        return capacity;
+}
+
+//Returns the direction of the flow when given starting vertex
+Direction Edge::getDirection(int start)
+{
+    if(start == u)
+        return UV;
+    else
+        return VU;
+}
+
+/* Performs a BFS on the graph, returning true if path is found.
+Path is updated by reference */
+bool BFS(Graph g, pair<int, int> path[])
+{
+    queue<int> q;
+    bool visited[g.V] = {false};
+    
+    //Init by pushing source
+    q.push(source);
+    visited[source] = true; //Each vertex is only visited once
+    path[source].first = path[source].second = NIL; //Path saves the parent that accessed them and edge used
+    
+    while(!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for(int e : g.nodes[u].edges)
+        {
+            int v = g.edges[e].getOppositeEnd(u);
+            Direction dir = g.edges[e].getDirection(v);
+
+            //v must not be visited and edge must not be full
+            if(!visited[v] && g.edges[e].canPush(dir)) 
+            {
+                if(v == target) //Exit condition
+                {
+                    path[v].first = u; //v was visited by u
+                    path[v].second = e; //Visit used edge e
+
+                    return true;
+                }
+                else
+                {
+                    q.push(v);
+                    visited[v] = true;
+                    path[v].first = u; //v was visited by u
+                    path[v].second = e; //Visit used edge e
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+/* Applies Edmonds Karp algorithm to push flux.
+Returns total amount pushed */
+int edmondsKarp(Graph g)
+{
+    int maxFlow = 0;
+
+    /* Path is a sequence of parent indexes
+    path[u] = v means v is u's parent */
+    pair<int, int> path[g.V]; 
+
+    while(BFS(g, path))
+    {
+
+        for(Edge e : g.edges)
+        {
+            printf("Start: %d  End: %d   Capacity:%d   Flow:%d   Direction:%d\n", e.u, e.v, e.capacity, e.flow, e.d);
+        }
+
+        int minimum = findPathMinimum(g, path);
+        g.pushPathFlow(path, minimum);
+        maxFlow += minimum;
+
+    }
+
+    return maxFlow;
+}
+
+/* Returns the least amount of flow you can push through the path */
+int findPathMinimum(Graph g, pair<int, int> path[])
+{
+    int m = NIL;
+
+    //Iterates through each pair
+    for(pair<int, int> current = path[g.V-1]; current.first != NIL; current = path[current.first])
+    {
+        if(m == NIL)
+        {
+            Direction dir = g.edges[current.second].getDirection(path[current.first].first); //Parent to son direction
+            m = g.edges[current.second].getAvailableFlow(dir);
+        }
+        else
+        {
+            Direction dir = g.edges[current.second].getDirection(path[current.first].first);
+            m = min(g.edges[current.second].getAvailableFlow(dir), m);
+        }
+    }
+
+    return m;
+}
+
+/* Pushes the given flow throughout the path */
+void Graph::pushPathFlow(pair<int,int> path[], int n)
+{
+    for(pair<int, int> current = path[V-1]; current.first != NIL; current = path[current.first])
+    {
+        Direction dir = edges[current.second].getDirection(path[current.first].first);
+        edges[current.second].pushFlow(n, dir);
+    }
+}
+
+/* Returns the other end of the edge given one end*/
+int Edge::getOppositeEnd(int vertex)
+{
+    if(vertex == u)
+    {
+        return v;
+    }
+    else
+        return u;
+}
+
